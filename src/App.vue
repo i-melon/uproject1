5d4469aa48b7790bd8740dfc187f27f75d882167
Melon West <i.melonpub@gmail.com>
2024-03-01 17:29:17 +0500

    project1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <textarea :class="{red:!howManyLost}" v-model="message" @input="check"></textarea>
+    <p>{{ howManyLost.toString() + '/' + limit.toString()}}</p>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      message: 'string',
+      limit: 50,
+      key: false
+    }
+  },
+  computed:{
+    howManyLost:function(){
+      return this.limit - this.message.length
+    },
+    limitedMessage:function (){
+      return this.message.substring(0,51)
+    }
+  },
+  methods:{
+    check(){
+      if (this.howManyLost<0){
+        this.message = this.message.substring(0,this.limit)
+        this.key = true
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.red{
+  border: red solid 2px;
 }
 </style>
