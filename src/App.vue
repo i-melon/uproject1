fd0e03edfbcb2e01e338c42b849acbe13fb1d80c
Melon West <i.melonpub@gmail.com>
2024-03-04 16:22:11 +0500

    project 2

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,35 @@
 <template>
   <div>
-    <textarea :class="{red:!howManyLost}" v-model="message" @input="check"></textarea>
-    <p>{{ howManyLost.toString() + '/' + limit.toString()}}</p>
-
+    <my-input label="price" v-model="price" ></my-input>
+    <my-input label="shipping" v-model="shipping"></my-input>
+    <my-input label="discount" v-model="discount"></my-input>
+    <p>total cost: {{total}}</p>
   </div>
 </template>
 
 <script>
 
+
+import MyInput from "@/components/MyInput.vue";
+
 export default {
+  components: {MyInput},
   data(){
     return{
-      message: 'string',
-      limit: 50,
-      key: false
+      price: 0,
+      shipping: 0,
+      discount: 0
     }
   },
   computed:{
-    howManyLost:function(){
-      return this.limit - this.message.length
-    },
-    limitedMessage:function (){
-      return this.message.substring(0,51)
-    }
-  },
-  methods:{
-    check(){
-      if (this.howManyLost<0){
-        this.message = this.message.substring(0,this.limit)
-        this.key = true
-      }
+    total:function (){
+      return Number(this.price) + Number(this.shipping) - Number(this.discount)
     }
   }
+
 }
 </script>
 
 <style>
-.red{
-  border: red solid 2px;
-}
+
 </style>
---
+++ src/components/MyInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="item">
+    <label>{{label}}</label>
+    <input :value="modelValue" @input="send">
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    label:[String],
+    modelValue:[Number]
+  },
+  methods:{
+    send(event){
+      this.$emit('update:modelValue', event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
